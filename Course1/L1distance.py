accc226418de789d99a6315de873e585b1ed5d7b
Frank2017 <liulei_frank@126.com>
2017-01-17 21:27:02 +0800

    Established on Windows.

--- Course1/L1distance.py
+++ Course1/L1distance.py
@@ -1,5 +1,7 @@
-import  numpy as np
+import numpy as np
+import pickle
 import os
+
 
 # def unpickle(file):
 #     fileopen = open(file, 'rb')
@@ -7,12 +9,43 @@ import os
 #     fileopen.close()
 #     return dict
 
-data_dir = "D:\PYProjects"
-cifar_path = os.path.join(data_dir, "cifar-10-batches-py")
-file_path = os.path.join(cifar_path, "data_batch_1")
+
+width = 32
+height = 32
+channel = 3
+
+
+def readBinFile(filepath):
+    binfile = open(file_path, mode='rb')
+    filedata = binfile.read()
+    filesize = binfile.tell()
+    filedata2 = bytearray(filedata)
+    Xlen = width * height * channel + 1
+    cnt = 0
+    temp = []
+    picdata = []
+    for i in range(0,filesize):
+        if cnt == Xlen:
+            cnt = 0
+            picdata.append(temp)
+            temp.clear()
+        if cnt < Xlen:
+            temp.append(filedata2[i])
+            cnt += 1
+    picdata.append(temp)
+    return picdata
+
+
+data_dir = "/home/frank_ai/Desktop/"
+cifar_path = os.path.join(data_dir, "cifar-10-batches-bin")
+file_path = os.path.join(cifar_path, "data_batch_1.bin")
+class_path = os.path.join(cifar_path, "batches.meta.txt")
 if __name__ == '__main__':
-    # dict = unpickle("D:\PYProjects\cifar-10-batches-py\data_batch_1")
+    # dict = unpickle(file_path)
     print(file_path)
-    dict = np.load(file_path)
-    print(len(dict))
+    data = readBinFile(file_path)
+    lables = np.loadtxt(class_path, dtype="bytes").astype('str')
+    print(len(data))
+    print(data[9998][0])
+    print(lables)
 
