39cf5e3256a32acbb49996842e45acdb7672dcd8
Frank2017 <liulei_frank@126.com>
2017-01-17 18:34:29 +0800

    Established on Windows.

--- Course1/L1distance.py
+++ Course1/L1distance.py
@@ -1,15 +1,18 @@
-# _*_ coding:utf-8_*_
 import  numpy as np
-import pickle
+import os
 
-def unpickle(file):
-    fileopen = open(file, 'rb')
-    dict = pickle.load(fileopen)
-    fileopen.close()
-    return dict
+# def unpickle(file):
+#     fileopen = open(file, 'rb')
+#     dict = pickle.load(fileopen)
+#     fileopen.close()
+#     return dict
 
-
+data_dir = "D:\PYProjects"
+cifar_path = os.path.join(data_dir, "cifar-10-batches-py")
+file_path = os.path.join(cifar_path, "data_batch_1")
 if __name__ == '__main__':
-    dict = unpickle(".//cifar-10-batches-py//data_batch_1")
-    len(dict)
+    # dict = unpickle("D:\PYProjects\cifar-10-batches-py\data_batch_1")
+    print(file_path)
+    dict = np.load(file_path)
+    print(len(dict))
 
