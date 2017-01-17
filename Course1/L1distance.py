e20859aaaf07c1721a3b08afedda501b875d93c0
Frank2017 <liulei_frank@126.com>
2017-01-17 18:20:01 +0800

    Established on Windows.

--- Course1/L1distance.py
+++ Course1/L1distance.py
@@ -1,2 +1,15 @@
 # _*_ coding:utf-8_*_
 import  numpy as np
+import pickle
+
+def unpickle(file):
+    fileopen = open(file, 'rb')
+    dict = pickle.load(fileopen)
+    fileopen.close()
+    return dict
+
+
+if __name__ == '__main__':
+    dict = unpickle(".//cifar-10-batches-py//data_batch_1")
+    len(dict)
+
